05a68585f9bd65863806cbeec38e33ef726d2434
vabeledo <abeledovictor@gmail.com>
2023-01-19 20:22:11 +0100

    remove unnecesary if condition

--- dpk.js
+++ dpk.js
@@ -10,12 +10,8 @@ function deterministicPartitionKey(event) {
   } else if(event) {
     const data = JSON.stringify(event);
     candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-  }
-
-  if(candidate && typeof candidate !== "string") {
-    candidate = JSON.stringify(candidate);
-  } else if(!candidate) {
-    candidate = TRIVIAL_PARTITION_KEY;
+  } else {
+    candidate = TRIVIAL_PARTITION_KEY; 
   }
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
