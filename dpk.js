b71b37f6b596fc36b17030d47f5d97f57af16e58
vabeledo <abeledovictor@gmail.com>
2023-01-19 20:24:27 +0100

    remove else statement in favor of candidate initialization with default value

--- dpk.js
+++ dpk.js
@@ -3,15 +3,13 @@ const crypto = require("crypto");
 function deterministicPartitionKey(event) {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let candidate = TRIVIAL_PARTITION_KEY;
 
   if(event && event.partitionKey) {
     candidate = event.partitionKey;
   } else if(event) {
     const data = JSON.stringify(event);
     candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY; 
   }
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
