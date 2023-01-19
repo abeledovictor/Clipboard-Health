615ddb0e19c8f878e83468eb7169df4f71f79d60
vabeledo <abeledovictor@gmail.com>
2023-01-19 20:32:20 +0100

    do not repeat logic | move create sha logic to a helper function

--- dpk.js
+++ dpk.js
@@ -1,5 +1,9 @@
 const crypto = require("crypto");
 
+function createSha(data) {
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+}
+
 function deterministicPartitionKey(event) {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
@@ -9,11 +13,11 @@ function deterministicPartitionKey(event) {
     candidate = event.partitionKey;
   } else if(event) {
     const data = JSON.stringify(event);
-    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+    candidate = createSha(data);
   }
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    candidate = createSha(candidate);
   }
   return candidate;
 }
